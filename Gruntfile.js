7db96e0f373417e473f6a5fe6d8126035624e43e
Manish Kumar <Manish Kumar>
2017-01-18 14:18:33 +0530

    rbo

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,6 +166,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
-    //grunt.registerTask('default', ['imagemin']);
+    //grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+    grunt.registerTask('default', ['cssmin']);
 };
